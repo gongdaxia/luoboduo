f1d88100a93687d484766f984715c50b1a2302a6
gongdaxia <vaststarsea@foxmail.com>
2017-03-31 20:15:03 +0800

    首页最新职位轮播

--- script/controller/home.js
+++ script/controller/home.js
@@ -4,7 +4,7 @@
 app.controller('homeCtrl', function ($scope, $state, $rootScope, professionService) {
     var vm = this;
     vm.params = $state.params;
-    vm.params.size = 4;
+    vm.params.size = 20;
     professionService.getBanner(0).then(function (res) {
         // 获取banner图
         if (res.data.code == 0) {
@@ -15,5 +15,20 @@ app.controller('homeCtrl', function ($scope, $state, $rootScope, professionServi
     $('.carousel').carousel({
         // 轮播图自动轮播定时器
         interval: 2000
+    });
+    professionService.getProfession(1,vm.params).then(function (res) {
+        if (res.data.code==0){
+            vm.professionInfo = res.data.data;
+            var bannerArr = [];
+            for (i = 0; i < 5; i++) {
+                bannerArr[i] = vm.professionInfo.slice(0, 4);
+                vm.professionInfo.splice(0, 4)
+            }
+            console.log(bannerArr);
+            $scope.homeProsBanner=bannerArr;
+            $('.carousel').carousel({
+                interval: 2000
+            })
+        }
     })
 });
